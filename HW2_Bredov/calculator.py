83e40062121c47daa9c82f48ed1cf77b10033594
Bredov D. V <d2707bredov@gmail.com>
2023-09-13 00:49:54 +0300

    Initial commit

---
+++ HW2_Bredov/calculator.py
@@ -0,0 +1,13 @@
+def main():
+#   eq = [int(i) if i.isdigit() else i for i in input().split()]
+    eq = input().split()
+    match eq[1]:
+        case "+":
+            return add(int(eq[0]), int(eq[2]))
+        case "-":
+            return subtract(int(eq[0]), int(eq[2]))
+        case "*":
+            return multiply(int(eq[0]), int(eq[2]))
+        case "/":
+            return divide(int(eq[0]), int(eq[2]))
+main()
