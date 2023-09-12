bbe2e1758aa5af5c6aceb21db75ab1f7f28fe022
Anastasia <anastasi.amber@gmail.com>
2023-09-12 21:04:03 +0300

    add main

--- HW2_Gorbarenko/calculator.py
+++ HW2_Gorbarenko/calculator.py
@@ -2,6 +2,22 @@ a,b,c = input().split()
 a = int(a)
 c = int(c)
 
+a,b,c = input().split()
+a = int(a)
+c = int(c)
+
+if (b == "+"): #sokol_sum
+  print(sokol_sum(a,c))
+elif (b == "-"): #delta
+  print(delta(a,c))
+elif (b == "*"): #multi
+  print(multi(a,c))
+elif (b == "/") : #teilen
+  print(teilen(a,c))
+else:
+  print("I dont know what it is")
+
+
 def teilen(a,b):
     return(a / b)
 
