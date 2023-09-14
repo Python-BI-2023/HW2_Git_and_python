988b3efeca2dc36143cff990144dc541e216b835
Liza <root@Soft-Machine>
2023-09-14 23:59:43 +0700

    Create calculator.py and determine main function

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def main():
+    a, znak, b = input().split()
+    a = float(a)
+    b = float(b)
+    if znak == "+":
+        print(summa(a, b))
+    elif znak == "-":
+        print(raznost(a, b))
+    elif znak == "*":
+        print(multiply(a, b))
+    elif znak == "/":
+        print(delenie(a, b))
