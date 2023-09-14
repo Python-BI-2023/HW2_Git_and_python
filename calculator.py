a53ac448d9a22061a063d9032e6e162e9ff4a7a8
zhurkv <zhur_kv@mail.ru>
2023-09-15 00:36:35 +0300

    Add summation

--- calculator.py
+++ calculator.py
@@ -1,3 +1,15 @@
 def multiplication(a,b):
     answer = a * b
     return answer
+
+def summing (a, b):
+    return a + b
+
+expression = input ("Введите выражение вида 1 + 1 ")
+nums = expression.split()
+num1 = float(nums[0])
+operator = nums[1]
+num2 = float(nums[2])
+if operator == '+':
+    result = summing(num1, num2)
+print (result)
