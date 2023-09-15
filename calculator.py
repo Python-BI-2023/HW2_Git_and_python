1a261775688aba8222e4ca96da97980c5d9f8fc8
Aleksandr Voskoboinikov <wwoskie@gmail.com>
2023-09-15 18:14:43 +0300

    add parser and input

--- calculator.py
+++ calculator.py
@@ -1,13 +1,25 @@
-calculator_func_dict = {'+': addition, 
-                        '-': substraction, 
-                        '*': multiplication, 
-                        '/': division}
-
-while command != 'exit':
-    inp = input('Enter your expression:')
-
 def multiply(a,b):
     return a*b
 
 def subtraction(a, b):
     return a - b 
+
+def calc_parser(calc_string):
+    calc_list = calc_string.split(' ')
+    calc_list[0] = float(calc_list[0])
+    calc_list[2] = float(calc_list[0])
+    return calc_list
+
+calc_string = input('Enter your expression:')
+command = calc_parser(calc_string)[1]
+
+while inp != 'exit':
+    if command in calculator_func_dict:
+        print(calculator_func_dict[command](calc_string[0], calc_string[2]))
+    else:
+        print('Seems like an invalid expression. Please, enter valid expression!')
+
+    calc_string = input('Enter your expression:')
+    command = calc_parser(calc_string)[1]
+
+print('See you next time!')
