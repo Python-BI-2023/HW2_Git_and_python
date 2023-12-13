ad4c614e8505eb8f66ae6c43ecc8f0e505caaf0e
Ivan Kozin <brayin.2016@gmail.com>
2023-12-13 14:44:22 +0300

    Add subtraction function. Modify main function

--- calculator.py
+++ calculator.py
@@ -1,22 +1,33 @@
-'''
+"""
 It's a calculator tool.
-'''
+"""
+
+
+def addition(num1, num2):
+    return num1 + num2
+
+
+def subtraction(num1, num2):
+    return num1 - num2
+
 
 OPERATIONS = {
-    '+' : addition(num1, num2),
-    '-' : subtraction,
-    '*' : multiplication,
-    '/' : division,
-    ':' : division
-}
+    '+': addition,
+    '-': subtraction}
+
+
+# '*' : multiplication,
+# '/' : division,
+# ':' : division
 
 
 def main():
-    input_ = input('Введите математическую операцию').split()
+    input_ = input('Введите математическую операцию: \n').split()
     num1 = float(input_[0])
     num2 = float(input_[2])
     operator = input_[1]
-    result = 0
-
-    OPERATIONS[operator]
+    result = OPERATIONS[operator](num1, num2)
     print(result)
+
+
+main()
